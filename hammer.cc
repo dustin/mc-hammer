94369a6b1ddb12b56966b2ea42db45e8600cb48e
Dustin Sallings <dustin@spy.net>
2010-05-14 12:56:41 -0700

    globals--

--- hammer.cc
+++ hammer.cc
@@ -14,11 +14,6 @@
 
 int keyn = 0;
 
-char bigassbuffer[MAX_SIZE];
-
-memcached_st *memc;
-memcached_server_st *servers;
-
 class Item {
 public:
     Item(const char *k) : key(k), len(0) {
@@ -31,46 +26,72 @@ public:
     }
 
     std::string key;
-    size_t len;
+    size_t      len;
 };
 
-static void send(Item &i) {
-    memcached_return rc = memcached_set(memc,
-                                        i.key.c_str(), i.key.length(),
-                                        bigassbuffer, i.len,
-                                        0, 0);
-    if (rc != MEMCACHED_SUCCESS) {
-        std::cerr << "Error setting " << i.key << ": "
-                  << memcached_strerror(memc, rc) << std::endl;
+class MCHammer {
+public:
+
+    MCHammer(const char *servers,
+             size_t nitems,
+             size_t mincr,
+             size_t msize) : num_items(nitems),
+                             max_incr(mincr),
+                             max_size(msize),
+                             memc(NULL),
+                             bigassbuffer(NULL) {
+
+        memc= memcached_create(NULL);
+        assert(memc);
+
+        memcached_server_st *srvrs = memcached_servers_parse(servers);
+        memcached_server_push(memc, srvrs);
+        memcached_server_list_free(srvrs);
+
+        bigassbuffer = static_cast<char *>(malloc(sizeof(char) * max_size));
+        assert(bigassbuffer);
+
+        for (int i = 0; i < sizeof(bigassbuffer); ++i) {
+            bigassbuffer[i] = 0xff & rand();
+        }
+
     }
-}
 
-static void process(std::vector<Item> &items) {
-    while (true) {
-        std::vector<Item>::iterator it;
-        for (it = items.begin(); it != items.end(); ++it) {
-            Item &i = *it;
+    void hurtEm(std::vector<Item> &items) {
+        while (true) {
+            std::vector<Item>::iterator it;
+            for (it = items.begin(); it != items.end(); ++it) {
+                Item &i = *it;
 
-            send(i);
+                send(i);
 
-            i.incrementSize();
+                i.incrementSize();
+            }
         }
     }
-}
+
+private:
+
+    void send(Item &i) {
+        memcached_return rc = memcached_set(memc,
+                                            i.key.c_str(), i.key.length(),
+                                            bigassbuffer, i.len,
+                                            0, 0);
+        if (rc != MEMCACHED_SUCCESS) {
+            std::cerr << "Error setting " << i.key << ": "
+                      << memcached_strerror(memc, rc) << std::endl;
+        }
+    }
+
+    size_t        num_items;
+    size_t        max_incr;
+    size_t        max_size;
+    memcached_st *memc;
+    char * bigassbuffer;
+};
 
 int main(int argc, char **argv) {
     std::vector<Item> items;
-
-    memc= memcached_create(NULL);
-    assert(memc);
-
-    servers= memcached_servers_parse(argv[--argc]);
-    memcached_server_push(memc, servers);
-    memcached_server_list_free(servers);
-
-    for (int i = 0; i < sizeof(bigassbuffer); ++i) {
-        bigassbuffer[i] = 0xff & rand();
-    }
 
     for (int i = 0; i < NUM_ITEMS; ++i) {
         char buf[32];
@@ -79,5 +100,9 @@ int main(int argc, char **argv) {
         items.push_back(Item(buf));
     }
 
-    process(items);
+    MCHammer hammer("localhost:11211",
+                    NUM_ITEMS,
+                    MAX_INCR,
+                    MAX_SIZE);
+    hammer.hurtEm(items);
 }
